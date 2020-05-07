0bd850c55018373b718f7e4848e006b28d79e61f
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-05-06 23:30:47 -0300

    Code refactoring

--- src/index.js
+++ src/index.js
@@ -5,43 +5,33 @@ const promotions = {
 	fullLook: 'FULL LOOK'
 };
 
-
 function getShoppingCart(ids, productsList) {
-
 	const productsSummary = generateProductsSummary(ids, productsList);
 	const promotion = getPromotionType(countDistinctCategories(productsSummary));
 	const bill = calculateBill(productsSummary, promotion);
 
 	const getDiscountPercentage = () => `${ ((bill.accumulatedDiscount * 100) / bill.fullPrice).toFixed(2) }%`;
 
-	productsSummary.forEach(product => {
-		delete product.tempPrices;
-	});
-
 	return {
-		products: productsSummary,
+		products: refineProductsSummary(productsSummary),
 		promotion: promotion,
 		totalPrice: (bill.fullPrice - bill.accumulatedDiscount).toFixed(2),
 		discountValue: (bill.accumulatedDiscount).toFixed(2),
 		discount: getDiscountPercentage()
 	};
-
 }
 
 
 function calculateBill(productsSummary, promotion) {
-
 	const calculatedBill = productsSummary.reduce((bill, product) => {
 		bill.fullPrice += product.tempPrices['regularPrice'];
-		// Checks for discounts and add to the object
-		if(product.tempPrices[promotion] != undefined) {
+
+		if(product.tempPrices[promotion] != undefined)
 			bill.accumulatedDiscount += (product.tempPrices['regularPrice'] - product.tempPrices[promotion]);
-		}
+
 		return bill;
 
 	}, { fullPrice: 0.0, accumulatedDiscount: 0.0 });
-
-
 
 	return calculatedBill;
 }
@@ -52,20 +42,31 @@ function generateProductsSummary(ids, productsList) {
 	let product;
 
 	ids.forEach(id => {
-		// Finds the product with the Id
+		// Finds the product with the given Id
 		product = productsList.find(p => p.id == id);
-
+		// Adds product
 		productsSummary.push({ 
 			name: product.name,
 			category: product.category,
-			tempPrices:	{
-					regularPrice: product.regularPrice,
-					'SINGLE LOOK': tryGetPromotionPrice(promotions.singleLook, product) ,
-					'DOUBLE LOOK': tryGetPromotionPrice(promotions.doubleLook, product),
-					'TRIPLE LOOK': tryGetPromotionPrice(promotions.tripleLook, product),
-					'FULL LOOK': tryGetPromotionPrice(promotions.fullLook, product),
-				}
+			tempPrices: 
+			{
+				regularPrice: product.regularPrice,
+				'SINGLE LOOK': tryGetPromotionPrice(promotions.singleLook, product) ,
+				'DOUBLE LOOK': tryGetPromotionPrice(promotions.doubleLook, product),
+				'TRIPLE LOOK': tryGetPromotionPrice(promotions.tripleLook, product),
+				'FULL LOOK': tryGetPromotionPrice(promotions.fullLook, product)
+			}
 		});
+	});
+
+	return productsSummary;
+}
+
+
+function refineProductsSummary(productsSummary) {
+	
+	productsSummary.forEach(product => {
+		delete product.tempPrices;
 	});
 
 	return productsSummary;
@@ -76,16 +77,14 @@ function tryGetPromotionPrice(promotionName, product) {
 	const promotions = product.promotions;
 	let promotion;
 
-	/* Iterates over the promotions available for the product trying to find 
-	 * the desired promotion and returns its price, otherwise, 'undefined'
+	/* Iterates over the product's available promotions trying to find the given promotion.
+	 	If found, its price is returned, otherwise, 'undefined'
 	*/
 	 for(let i = 0; i < promotions.length; i++) {
 		promotion = promotions[i];
-
 		if(promotion['looks'].includes(promotionName)) 
 			return promotion['price'];
 	}
-
 	return undefined;
 }
 
@@ -103,7 +102,6 @@ function countDistinctCategories(productsSummary) {
 	return distinctCategories.length;
 }
 
-
 function getPromotionType(countDistinctCategories) {
 	switch(countDistinctCategories) {
 		case 1: return promotions.singleLook;
@@ -112,6 +110,5 @@ function getPromotionType(countDistinctCategories) {
 		default: return promotions.fullLook;
 	}
 }
-
 
 module.exports = { getShoppingCart };
